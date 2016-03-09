1cc0b832f135a0335a4211d67e56c6ee6f4b24ed
Robert Wagner <rwwagner@vt.edu>
2016-03-09 08:19:42 -0500

    Add ember-in-viewport

--- addon/components/ember-typewriter/component.js
+++ addon/components/ember-typewriter/component.js
@@ -1,8 +1,9 @@
 import Ember from 'ember';
 import $ from 'jquery';
+import InViewportMixin from 'ember-in-viewport';
 
-export default Ember.Component.extend({
-  didInsertElement() {
+export default Ember.Component.extend(InViewportMixin,{
+  didEnterViewport() {
     $('.' + this.get('class')).typed({
       strings: this.get('strings'),
       contentType: this.get('contentType') //'html' or 'text'
