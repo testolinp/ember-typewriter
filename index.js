98bc075eecedade8d21f81049b3a33239b18e8b0
Robert Wagner <rwwagner90@gmail.com>
2016-12-20 13:42:28 -0500

    Switch to npm

--- index.js
+++ index.js
@@ -5,17 +5,17 @@ module.exports = {
   name: 'ember-typewriter',
   included: function(app) {
     this._super.included.apply(this, arguments);
-
-    // see: https://github.com/ember-cli/ember-cli/issues/3718
-    if (typeof app.import !== 'function' && app.app) {
-      app = app.app;
-    }
-
-    if (!process.env.EMBER_CLI_FASTBOOT) {
-      if (app.env === "production") {
-        app.import(app.bowerDirectory + '/typed.js/dist/typed.min.js');
-      } else {
-        app.import(app.bowerDirectory + '/typed.js/js/typed.js');
+  },
+  options: {
+    nodeAssets: {
+      'typed.js': function() {
+        return {
+          enabled: !process.env.EMBER_CLI_FASTBOOT,
+          srcDir: 'dist',
+          import: [
+            'typed.min.js'
+          ]
+        };
       }
     }
   }
